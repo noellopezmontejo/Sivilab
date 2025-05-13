8f01b4a7f766ea34366542b7af97d342c7d01c1a
CRUZ NOEL LOPEZ MONTEJO <noellopez24@outlook.com>
2025-05-13 16:02:53 -0600

    13052023

---
+++ Sivilab.Models/Models/ResultValidaCorreo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sivilab.Models.Models
+{
+    public class ResultValidaCorreo
+    {
+        public int Validado { get; set; }
+
+    }
+}
