82319f02daa0ff5f2584e42abed284e19e0a540d
CRUZ NOEL LOPEZ MONTEJO <noellopez24@outlook.com>
2025-05-19 10:29:33 -0600

    19052025

---
+++ Sivilab.Portal/Controllers/EventController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sivilab.Portal.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
