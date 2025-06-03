1cd9c7998c433b75e747cac83361a284b3d9b773
Rodo Yamazaki <rodo@rodoarts.com>
2025-06-03 15:50:46 -0600

    03/Jun/2025

---
+++ Sivilab.Models/Models/Eventos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sivilab.Models.Models
+{
+    public class EventoModel
+    {
+        public required string EventName { get; set; }
+        public required string Description { get; set; }
+        public required String EventDate { get; set; }
+        //public required string Location { get; set; }
+        public required string ImageURL { get; set; }
+    }
+}
---
+++ Sivilab.Models/Models/Ofertas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sivilab.Models.Models
+{
+    public class OfertaModel
+    {
+        public required string  OfferName { get; set; }
+        public required string  Description { get; set; }
+        public required string  ImageURL { get; set; }
+        public int              Experience { get; set; }
+        public double           Salary { get; set; }
+    }
+}
