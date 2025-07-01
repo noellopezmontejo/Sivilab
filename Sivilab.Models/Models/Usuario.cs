6d6a111f6770bd6b04a6599cfe9dede44fbff001
CRUZ NOEL LOPEZ MONTEJO <noellopez24@outlook.com>
2025-07-01 16:02:46 -0600

    01072025

--- Sivilab.Models/Models/Usuario.cs
+++ Sivilab.Models/Models/Usuario.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Sivilab.Models.Models
 {
     public class Usuario
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Email { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }=string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }= string.Empty;
+
+        [Required]
         public string Contrasena { get; set; }
+        
+        [Required]
         public string Rol { get; set; }
         public bool Validado { get; set; }
-        public string CodigoValidacion { get; set; }
+        public string CodigoValidacion { get; set; } = string.Empty;
 
     }
 }
--- Sivilab.Web/Program.cs
+++ Sivilab.Web/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:5094/") });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
