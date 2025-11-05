c26c71d739f60a8772caf3d50e35a5de8ca49793
CRUZ NOEL LOPEZ MONTEJO <noellopez24@outlook.com>
2025-11-05 14:03:07 -0600

    05112025

---
+++ Sivilab.API/Controllers/CandidatoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sivilab.API.Controllers
+{
+    public class CandidatoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
